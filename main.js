238ee5088e0a25d7bea7b0be75c324eef095b0a3
Marcos Vinicius <mvcavalcante.com@gmail.com>
2023-08-10 16:06:36 -0300

    Atualizando o código

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
 
     $('form').on('submit', function(e) {
         e.preventDefault();
-
+        
         const adicionarTarefa = $('#adicionar-tarefa').val();
         const novaTarefa = $('<li></li>').text(adicionarTarefa);
+
+        $('tarefas-container').click(function() {
+            $('ul').addClass("#ul-tarefas");
+        })
 
         $('ul').append(novaTarefa);
         $(novaTarefa).fadeIn();
